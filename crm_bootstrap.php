6eddb02b6079a53ef0281176b042b9d1d6f4d15f
sebidlo <sebidlo@gmail.com>
2018-06-22 13:39:47 +0200

    CRM obiektowy - start

--- crm_bootstrap.php
+++ crm_bootstrap.php
@@ -11,13 +11,14 @@ $salesman2 = new Salesman(2, "Jakis facet");
 $salesman3 = new Salesman(3, "Jakaś kobieta");
 
 $company = new Company(1, "Firma AAAA");
-$company2 = new Company(2, "firma 222");
+$company2 = new Company(2, "Firma 2");
+$company3 = new Company(3, "Firma 3");
 
 
 $activities = array (
 new Activity(1, "Nawiązanie kontaktu", new DateTime("2018-02-01 13:00"), $company, $salesman, ActivityType::PHONE, ActivityStatus::CLOSED, "notatka"),
 new Activity(2, "Nawiązanie kontaktu", new DateTime("2018-03-01 14:00"), $company2, $salesman2, ActivityType::METTONG, ActivityStatus::CLOSED, "notatka JALDANDNADA"),
-new Activity(3, "Nawiązanie kontaktu", new DateTime("2018-03-01 14:00"), $company, $salesman3, ActivityType::METTONG, ActivityStatus::IN_PROGRES, "notatka JALDANDNADA")
+new Activity(3, "Nawiązanie kontaktu", new DateTime("2018-03-01 14:00"), $company3, $salesman3, ActivityType::EMAIL, ActivityStatus::IN_PROGRES, "notatka JALDANDNADA")
 );
 
 //echo $activity->getInfo();
